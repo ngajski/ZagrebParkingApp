27d9eb3fee20a2b249e85c8237b20910eda93392
Martin Pandek <pandek.martin@gmail.com>
2017-03-09 19:58:36 +0100

    pronalazenje zone za ocitane koordinate... svi testovi koje sam napravio su prosli... samo treba kasnije sloziti slanje koordinata s kojima ce radit

---
+++ app/src/main/java/hr/fer/zagrebparkingapp/FindZone.java
@@ -0,0 +1,99 @@
+package hr.fer.zagrebparkingapp;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Pandek on 9.3.2017..
+ */
+
+public class FindZone {
+
+    public static void main(String[] args ) throws IOException {
+        //double latitude = 0, longitude = 0;
+        String zona = provjeri(45.7940, 16.0007);
+        System.out.print(zona);
+    }
+
+    public static String provjeri (double latitude, double longitude) throws IOException{
+        for(int i = 0; i < 4; i++){//6 zona
+
+            if(i == 0){
+                System.setIn(new FileInputStream("C:/Users/Pandek/Desktop/treca.txt"));
+                BufferedReader ulaz = new BufferedReader(new InputStreamReader(System.in));
+                boolean nasao = unutra(latitude, longitude, ulaz);
+                if(nasao){
+                    return "treca";
+                }
+            } else if(i == 1){
+                System.setIn(new FileInputStream("C:/Users/Pandek/Desktop/cetiri_jedan.txt"));
+                BufferedReader ulaz = new BufferedReader(new InputStreamReader(System.in));
+                boolean nasao = unutra(latitude, longitude, ulaz);
+                if(nasao){
+                    return "cetiri_jedan";
+                }
+            } else if(i == 2){
+                System.setIn(new FileInputStream("C:/Users/Pandek/Desktop/cetiri_dva.txt"));
+                BufferedReader ulaz = new BufferedReader(new InputStreamReader(System.in));
+                boolean nasao = unutra(latitude, longitude, ulaz);
+                if(nasao){
+                    return "cetiri_dva";
+                }
+            } else if(i == 3){
+                System.setIn(new FileInputStream("C:/Users/Pandek/Desktop/druga.txt"));
+                BufferedReader ulaz = new BufferedReader(new InputStreamReader(System.in));
+                boolean nasao = unutra(latitude, longitude, ulaz);
+                if(nasao){
+                    return "druga";
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean unutra(double latitude, double longitude, BufferedReader ulaz) throws IOException{
+        String line = null;
+        boolean pao = false;
+        int linija = 0;
+        do{
+            line = ulaz.readLine().trim();
+            String[] podaci = line.split(" ");
+            if(podaci.length == 1){
+                linija = 0;
+                pao = false;
+            }
+            if(!(podaci.length == 1)){
+                linija++;
+                double lat = Double.parseDouble(podaci[1]);
+                double lon = Double.parseDouble(podaci[2]);
+                if((latitude > lat || longitude < lon) && linija == 1){
+                    pao = true;
+                    line = ulaz.readLine();
+                    line = ulaz.readLine();
+                    line = ulaz.readLine();
+                    continue;
+                } else if((latitude > lat || longitude > lon) && linija == 2){
+                    pao = true;
+                    line = ulaz.readLine();
+                    line = ulaz.readLine();
+                    continue;
+                } else if((latitude < lat || longitude < lon) && linija == 3){
+                    pao = true;
+                    line = ulaz.readLine();
+                    continue;
+                } else if((latitude < lat || longitude > lon) && linija == 4){
+                    pao = true;
+                    continue;
+                } else {
+                    if(linija == 4 && !pao)
+                        return true;
+                }
+            }
+
+        } while(!line.equals("kraj"));
+
+        return false;
+    }
+}
