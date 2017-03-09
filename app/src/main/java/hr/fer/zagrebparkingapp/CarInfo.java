d1170d99b36a209d6f1be9707651e8f8869155e3
DESKTOP-H7G5SE6\Martin <martin.mihalic@fer.hr>
2017-03-09 16:27:43 +0100

    Netestirana verzija bez Google podrške

---
+++ app/src/main/java/hr/fer/zagrebparkingapp/CarInfo.java
@@ -0,0 +1,8 @@
+package hr.fer.zagrebparkingapp;
+
+/**
+ * Created by Martin on 09-Mar-17.
+ */
+
+public class CarInfo {
+}
---
+++ app/src/main/java/hr/fer/zagrebparkingapp/CarsActivity.java
@@ -0,0 +1,13 @@
+package hr.fer.zagrebparkingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cars);
+    }
+}
